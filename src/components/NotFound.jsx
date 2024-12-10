ffc0d60613ad8001194bcb51d2ccb8374d5f2d65
Farhan-L4h <muhanmedan06@gmail.com>
2024-12-10 10:37:06 +0700

    Update Tampilan Dashboard

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -17,10 +17,10 @@ export default function Notfound() {
               home page.{" "}
             </p>
             <a
-              href="/"
+              href="javascript:history.back()"
               class="inline-flex text-white bg-blue-400 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4"
             >
-              Back to Homepage
+              Return Back
             </a>
           </div>
         </div>
